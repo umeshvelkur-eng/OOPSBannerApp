7a8a39b7cffd5089e230f993387e267a4dbdcf2d
Umesh <umeshvelkur@gmail.com>
2026-02-26 14:54:42 +0530

    Update UC4.java

--- UC4.java
+++ UC4.java
@@ -1,12 +1,19 @@
-public static void main(String[] args) {
-		
-        System.out.println("   *        *     **       ***     ");
-        System.out.println("  *  *     * *    *    *   **          ");
-        System.out.println(" *    *   *   *   *    *  **           ");
-        System.out.println(" *    *   *   *   *    *   **          ");
-        System.out.println(" *    *   *   *   **        **     ");
-        System.out.println(" *    *   *   *   *                *  ");
-        System.out.println(" *    *   *   *   *                 *   ");
-        System.out.println("  *  *     * *    *                *    ");
-        System.out.println("   *        *     *           **    ");
+public class UC4 {
+
+    public static void main(String[] args) {
+
+        // Banner stored in String Array
+        String[] banner = {
+            "*******  *******  ******   *******",
+            "*     *  *     *  *     *  *      ",
+            "*     *  *     *  ******   *******",
+            "*     *  *     *  *              *",
+            "*******  *******  *        *******"
+        };
+
+        // Loop to print banner
+        for(String line : banner) {
+            System.out.println(line);
+        }
+    }
 }
